052ce9969baacedc8f74d1ebf43f9bed1ac6286e
FranciscoAbelCoronaLopez <franciscoabel.coronaLF8@comunidadunir.net>
2024-05-26 15:38:19 -0600

    Cambios Abel Corona

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ Organization: UNIR
 
 import os
 import sys
+from collections import defaultdict
 
 DEFAULT_FILENAME = "words.txt"
 DEFAULT_DUPLICATES = False
-
 
 def sort_list(items, ascending=True):
     if not isinstance(items, list):
@@ -16,10 +16,14 @@ def sort_list(items, ascending=True):
 
     return sorted(items, reverse=(not ascending))
 
-
 def remove_duplicates_from_list(items):
     return list(set(items))
 
+def count_occurrences(items):
+    occurrences = defaultdict(int)
+    for item in items:
+        occurrences[item] += 1
+    return occurrences
 
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
@@ -43,7 +47,23 @@ if __name__ == "__main__":
         print(f"El fichero {filename} no existe")
         word_list = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"]
 
+
+    word_occurrences = count_occurrences(word_list)
+
+
+    repeated_words = defaultdict(list)
+    for word, count in word_occurrences.items():
+        if count > 1:
+            repeated_words[count].append(word)
+
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
 
-    print(sort_list(word_list))
+    sorted_word_list = sort_list(word_list)
+    print(" ")
+    print("Lista ordenada de palabras:")
+    print(sorted_word_list)
+    print(" ")
+    for count, words in repeated_words.items():
+        print(f"Palabras que se repiten {count} veces:")
+        print(words)
